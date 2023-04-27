4b491b85f92cc17e6ef69b17b886116f8f862626
oscarm8 <oscar.roman229303@potros.itson.edu.mx>
2023-04-27 14:34:47 -0700

    - Agregado efecto de seleccion en botones de temas favoritos.

--- app/src/main/java/roman/oscar/lecturaapp/ConfigureProfileActivity.kt
+++ app/src/main/java/roman/oscar/lecturaapp/ConfigureProfileActivity.kt
@@ -4,14 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class ConfigureProfileActivity : AppCompatActivity() {
+    private lateinit var imgAvatar: ImageView
+    private lateinit var rightButton: Button
+    private lateinit var leftButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configure_profile)
-
         val btnNext = findViewById<Button>(R.id.btnNext)
+        var tvAge = findViewById<TextView>(R.id.tvAge)
+        var btnPlus = findViewById<Button>(R.id.btnPlus)
+        var btnMinus = findViewById<Button>(R.id.btnMinus)
+        var mAge = 12
+        imgAvatar = findViewById(R.id.imgAvatar);
+        rightButton = findViewById(R.id.btnRight);
+        leftButton = findViewById(R.id.btnLeft);
 
+        tvAge.text = mAge.toString()
         btnNext.setOnClickListener {
             val intent = Intent(this, FavoriteThemesActivity::class.java)
             startActivity(intent)
@@ -22,6 +35,31 @@ class ConfigureProfileActivity : AppCompatActivity() {
         btnBack.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+        rightButton.setOnClickListener {
+            val imageResources = intArrayOf(roman.oscar.lecturaapp.R.drawable.img_avatar_uno, roman.oscar.lecturaapp.R.drawable.avatarprofile, roman.oscar.lecturaapp.R.drawable.imagen_libro1)
+            val currentConstantState = imgAvatar.drawable.constantState?.newDrawable()?.constantState
+            val currentIndex = imageResources.indexOfFirst { resources.getDrawable(it).constantState?.equals(currentConstantState) == true } ?: 0
+            val nextIndex = (currentIndex + 1) % imageResources.size
+            imgAvatar.setImageResource(imageResources[nextIndex])
+        }
+        leftButton.setOnClickListener {
+            val imageResources = intArrayOf(roman.oscar.lecturaapp.R.drawable.img_avatar_uno, roman.oscar.lecturaapp.R.drawable.avatarprofile, roman.oscar.lecturaapp.R.drawable.imagen_libro1)
+            val currentConstantState = imgAvatar.drawable.constantState?.newDrawable()?.constantState
+            val currentIndex = imageResources.indexOfFirst { resources.getDrawable(it).constantState?.equals(currentConstantState) == true } ?: 0
+            val previousIndex = if (currentIndex == 0) imageResources.size - 1 else currentIndex - 1
+            imgAvatar.setImageResource(imageResources[previousIndex])
+        }
+        btnPlus.setOnClickListener {
+            mAge++
+            tvAge.text = mAge.toString()
+        }
+        btnMinus.setOnClickListener {
+            if (mAge == 0) {
+            } else {
+                mAge--
+                tvAge.text = mAge.toString()
+            }
         }
     }
 }
--- app/src/main/java/roman/oscar/lecturaapp/FavoriteThemesActivity.kt
+++ app/src/main/java/roman/oscar/lecturaapp/FavoriteThemesActivity.kt
@@ -1,6 +1,8 @@
 package roman.oscar.lecturaapp
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -15,6 +17,110 @@ class FavoriteThemesActivity : AppCompatActivity() {
         btnBack.setOnClickListener {
             val intent = Intent(this, ConfigureProfileActivity::class.java)
             startActivity(intent)
+        }
+
+        configurarBotones();
+
+    }
+
+
+    private fun configurarBotones() {
+        val btnSports = findViewById<Button>(R.id.btnSports)
+        val btnAnimals = findViewById<Button>(R.id.btnAnimals)
+        val btnMagic = findViewById<Button>(R.id.btnMagic)
+        val btnVideogames = findViewById<Button>(R.id.btnVideogames)
+        val btnMusic = findViewById<Button>(R.id.btnMusic)
+        val btnSpace = findViewById<Button>(R.id.btnSpace)
+
+        var btnSportsSeleccionado = false
+        var btnAnimalsSeleccionado = false
+        var btnMagicSeleccionado = false
+        var btnVideogamesSeleccionado = false
+        var btnMusicSeleccionado = false
+        var btnSpaceSeleccionado = false
+
+        btnSports.setOnClickListener {
+            if (btnSportsSeleccionado) {
+                btnSports.setBackgroundResource(R.drawable.ic_age)
+                btnSports.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_sports, 0, 0)
+                btnSports.setTextColor(Color.parseColor("#66607E")) //Color texto
+                btnSportsSeleccionado = false
+            } else {
+                btnSports.setBackgroundResource(R.drawable.gradient_gold_button)
+                btnSports.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_sports_selected, 0, 0)
+                btnSports.setTextColor(Color.parseColor("#715F20")) //Color texto
+                btnSportsSeleccionado = true
+            }
+        }
+
+        btnAnimals.setOnClickListener {
+            if (btnAnimalsSeleccionado) {
+                btnAnimals.setBackgroundResource(R.drawable.ic_age)
+                btnAnimals.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_animals, 0, 0)
+                btnAnimals.setTextColor(Color.parseColor("#66607E")) //Color texto
+                btnAnimalsSeleccionado = false
+            } else {
+                btnAnimals.setBackgroundResource(R.drawable.gradient_gold_button)
+                btnAnimals.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_animals_selected, 0, 0)
+                btnAnimals.setTextColor(Color.parseColor("#715F20")) //Color texto
+                btnAnimalsSeleccionado = true
+            }
+        }
+
+        btnMagic.setOnClickListener {
+            if (btnMagicSeleccionado) {
+                btnMagic.setBackgroundResource(R.drawable.ic_age)
+                btnMagic.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_magic, 0, 0)
+                btnMagic.setTextColor(Color.parseColor("#66607E")) //Color texto
+                btnMagicSeleccionado = false
+            } else {
+                btnMagic.setBackgroundResource(R.drawable.gradient_gold_button)
+                btnMagic.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_magic_selected, 0, 0)
+                btnMagic.setTextColor(Color.parseColor("#715F20")) //Color texto
+                btnMagicSeleccionado = true
+            }
+        }
+
+        btnVideogames.setOnClickListener {
+            if (btnVideogamesSeleccionado) {
+                btnVideogames.setBackgroundResource(R.drawable.ic_age)
+                btnVideogames.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_videogames, 0, 0)
+                btnVideogames.setTextColor(Color.parseColor("#66607E")) //Color texto
+                btnVideogamesSeleccionado = false
+            } else {
+                btnVideogames.setBackgroundResource(R.drawable.gradient_gold_button)
+                btnVideogames.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_videogames_selected, 0, 0)
+                btnVideogames.setTextColor(Color.parseColor("#715F20")) //Color texto
+                btnVideogamesSeleccionado = true
+            }
+        }
+
+        btnMusic.setOnClickListener {
+            if (btnMusicSeleccionado) {
+                btnMusic.setBackgroundResource(R.drawable.ic_age)
+                btnMusic.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_music, 0, 0)
+                btnMusic.setTextColor(Color.parseColor("#66607E")) //Color texto
+                btnMusicSeleccionado = false
+            } else {
+                btnMusic.setBackgroundResource(R.drawable.gradient_gold_button)
+                btnMusic.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_music_selected, 0, 0)
+                btnMusic.setTextColor(Color.parseColor("#715F20")) //Color texto
+                btnMusicSeleccionado = true
+            }
+        }
+
+        btnSpace.setOnClickListener {
+            if (btnSpaceSeleccionado) {
+                btnSpace.setBackgroundResource(R.drawable.ic_age)
+                btnSpace.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_space, 0, 0)
+                btnSpace.setTextColor(Color.parseColor("#66607E")) //Color texto
+                btnSpaceSeleccionado = false
+            } else {
+                btnSpace.setBackgroundResource(R.drawable.gradient_gold_button)
+                btnSpace.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_icon_space_selected, 0, 0)
+                btnSpace.setTextColor(Color.parseColor("#715F20")) //Color texto
+                btnSpaceSeleccionado = true
+            }
         }
     }
 }
