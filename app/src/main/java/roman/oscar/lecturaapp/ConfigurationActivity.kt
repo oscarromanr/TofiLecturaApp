01adb986fd0d5ae855e464088dde58284098ce1c
Fernando <fernando.bocardo228476@potros.itson.edu.mx>
2023-04-27 16:07:25 -0700

    Cerrar Sesion

--- app/src/main/java/roman/oscar/lecturaapp/ConfigurationActivity.kt
+++ app/src/main/java/roman/oscar/lecturaapp/ConfigurationActivity.kt
@@ -11,10 +11,18 @@ class ConfigurationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_configuration)
 
         val btnBack = findViewById<Button>(R.id.btnBack)
+        val btn_cerrar_sesion = findViewById<Button>(R.id.btn_cerrar_sesion)
 
         btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        btn_cerrar_sesion.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
