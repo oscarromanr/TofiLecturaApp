5a109cc2a1368bcc45d08ac210f574423360522b
CarlosAngulo30 <carlos.angulo228410@potros.itson.edu.mx>
2023-05-16 00:11:31 -0700

    Funcionalidad de buscar por categorias

--- app/src/main/java/roman/oscar/lecturaapp/Categoria.kt
+++ app/src/main/java/roman/oscar/lecturaapp/Categoria.kt
@@ -1,3 +1,3 @@
 package roman.oscar.lecturaapp
 
-data class Categoria (var nombre:String, var libros:ArrayList<Libro>, var image:Int)
+data class Categoria(var nombre:String, var libros: ArrayList<Libro>?, var image:Int)
--- app/src/main/java/roman/oscar/lecturaapp/Libro.kt
+++ app/src/main/java/roman/oscar/lecturaapp/Libro.kt
@@ -1,3 +1,12 @@
 package roman.oscar.lecturaapp
 
-data class Libro (var titulo:String, var image:String, var autor:String, var paginas: Int,var sinopsis:String, var categorias:ArrayList<String>)
+data class Libro(
+    var titulo: String,
+    var image: String,
+    var autor: String,
+    var paginas: Int,
+    var sinopsis: String,
+    var categorias: ArrayList<String>
+) {
+    constructor() : this("", "", "", 0, "", ArrayList())
+}
